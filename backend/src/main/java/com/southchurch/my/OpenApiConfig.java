a08f00a4fd6ae503e791f5783c675d06827b2580
Lucas Bacchi <lucasbacchi22@gmail.com>
2026-01-29 15:37:03 -0500

    add OpenApiConfig.java

---
+++ backend/src/main/java/com/southchurch/my/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.southchurch.my;
+
+import org.springdoc.core.utils.SpringDocUtils;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+@Configuration
+public class OpenApiConfig {
+
+    static {
+        // Tell Swagger: "Never ask the user for a Jwt object"
+        SpringDocUtils.getConfig().addRequestWrapperToIgnore(Jwt.class);
+    }
+}
